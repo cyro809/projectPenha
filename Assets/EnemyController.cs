80ddaf783640a916edfb288500f5c6c1085f3cb1
Cyro Guimaraes <cyro809@gmail.com>
2018-03-18 19:25:41 -0300

    Inimigo só persegue o player quando está no chão.

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour {
 	public Transform Player;
 	int MoveSpeed = 7;
+	bool onGround;
 
 	Rigidbody rB;
 	// Use this for initialization
 	void Start () {
 		rB = GetComponent<Rigidbody> ();
+		onGround = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt (Player);
-		rB.AddForce (transform.forward * MoveSpeed);
+		if (onGround) {
+			transform.LookAt (Player);
+			rB.AddForce (transform.forward * MoveSpeed);
+		}
 	}
 
 	void OnCollisionEnter (Collision col) {
@@ -24,6 +28,12 @@ public class EnemyController : MonoBehaviour {
 			Vector3 force = transform.position - col.transform.position;
 			force.Normalize ();
 			rB.AddForce (force * magnitude);
+		}
+
+		if (col.gameObject.name == "Plane") {
+			onGround = true;
+		} else {
+			onGround = false;
 		}
 	}
 }
