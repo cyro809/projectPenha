52ae268f3f26e57a1ee0e53328db87202fc052f2
Cyro Guimaraes <cyro809@gmail.com>
2019-11-06 23:54:08 -0300

    Enemies destroyed after been pushed out of arena.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MovingObject {
-	public GameObject Player;
+	GameObject player;
+	GameObject plane;
 	int MoveSpeed = 7;
 	bool onGround;
 	private float pushBackForce = 400;
 	// Use this for initialization
 	public override void Start () {
+		player = GameObject.FindWithTag ("Player");
+		plane = GameObject.FindWithTag ("Ground");
 		onGround = false;
 		base.Start ();
 	}
 
 	protected override void OnMove() {
 		if (onGround) {
-			transform.LookAt (Player.transform.position);
+			transform.LookAt (player.transform.position);
 			rB.AddForce (transform.forward * MoveSpeed);
 		}
+		checkIfOutOfArena ();
 
+	}
+
+	void checkIfOutOfArena() {
+		if (transform.position.y < plane.transform.position.y - 5)
+			Destroy (gameObject);
 	}
 
 	void OnCollisionEnter (Collision col) {
