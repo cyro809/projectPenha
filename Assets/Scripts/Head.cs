e1d485a4dfa4662d5d0ed8aba2ca346ffc80b9c3
Cyro Guimaraes <cyro809@gmail.com>
2019-11-23 22:15:29 -0300

    Android pre-alpha version.

--- Assets/Scripts/Head.cs
+++ Assets/Scripts/Head.cs
@@ -7,16 +7,24 @@ public class Head : MonoBehaviour {
 	private Camera mainCamera;
 
 	public GunController gun;
+	Vector3 firePos;
 
 	// Use this for initialization
 	void Start () {
 		mainCamera = FindObjectOfType<Camera> ();
+		Input.multiTouchEnabled = true;
+
 	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 
-		Ray cameraRay = mainCamera.ScreenPointToRay (Input.mousePosition);
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			firePos = Input.GetTouch (1).position;
+		} else {
+			firePos = Input.mousePosition;
+		}
+		Ray cameraRay = mainCamera.ScreenPointToRay (firePos);
 		Plane groundPlane = new Plane (Vector3.up, Vector3.zero);
 		float rayLength;
 
@@ -28,14 +36,42 @@ public class Head : MonoBehaviour {
 
 		transform.rotation = Quaternion.Euler (0, transform.eulerAngles.y, 0);
 
-		if (Input.GetMouseButtonDown (0)) {
+		if (DetectFireInput()) {
 			gun.isFiring = true;
 		} else {
 			gun.isFiring = false;
 		}
 
-		if (Input.GetMouseButtonUp (0)) {
+		if (DetectFireInputEnd()) {
 			gun.isFiring = false;
+		}
+	}
+
+	bool DetectFireInput() {
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			if (Input.GetTouch (1).phase == TouchPhase.Began) {
+				return true;
+			}
+			return false;
+		} else {
+			if (Input.GetMouseButtonDown (0)) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+	bool DetectFireInputEnd() {
+		if (SystemInfo.deviceType == DeviceType.Handheld) {
+			if (Input.GetTouch (1).phase == TouchPhase.Ended) {
+				return true;
+			}
+			return false;
+		} else {
+			if (Input.GetMouseButtonUp (0)) {
+				return true;
+			}
+			return false;
 		}
 	}
 }
