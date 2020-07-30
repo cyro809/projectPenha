d870e33d4230ca7d44f0e96d2a016c594af72735
Cyro Guimaraes <cyro809@gmail.com>
2020-07-29 23:04:01 -0300

    Shield fade over time.

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -5,22 +5,20 @@ using UnityEngine;
 public class Shield : MonoBehaviour
 {
     private float pushBackForce = 1650;
-    private float transparency = 0.1f;
+    public float fadeSpeed = 5/30;
     // Start is called before the first frame update
-    
     public bool activate;
     public GameObject shieldText;
-    TextMesh countDownText;
     void Start()
     {
-        countDownText = shieldText.GetComponent<TextMesh> ();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         Color col = gameObject.GetComponent<MeshRenderer> ().material.color;
-        col.a = transparency;
+        col.a -= Time.deltaTime * fadeSpeed;
         GetComponent<MeshRenderer>().material.color = col;
         gameObject.SetActive (activate);
     }
@@ -35,15 +33,13 @@ public class Shield : MonoBehaviour
     public void ActivateShield() {
         activate = true;
         gameObject.SetActive (true);
-        StartCoroutine(CountdownEnum(3));
+        StartCoroutine(CountdownEnum(5));
     }
 
     IEnumerator CountdownEnum(int seconds)
     {
         int count = seconds;
-       countDownText = shieldText.GetComponent<TextMesh> ();
         while (count > 0) {
-            countDownText.text = "" + count.ToString ();
             // display something...
             yield return new WaitForSeconds(1);
             count --;
