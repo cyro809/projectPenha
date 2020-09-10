171b672950b950a1e26b030d4fd271435cbe1616
Cyro Guimaraes <cyro809@gmail.com>
2020-09-10 00:32:38 -0300

    Adds target cursor. Adding player gun game object.

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Texture2D cursorTarget;
+    void Start()
+    {
+        Cursor.SetCursor(cursorTarget, new Vector2(16, 6), CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
