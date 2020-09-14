5d337f99222bb3eb63b54898f4d747a5ec647425
Cyro Guimaraes <cyro809@gmail.com>
2020-09-13 21:59:16 -0300

    Merge with branch 'chore/player-movement'.

--- Assets/Scripts/Body.cs
+++ Assets/Scripts/Body.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class Body : MovingObject {
 	public float maxSpeed = 5f;
     public float dashSpeed = 300f;
+	public float acceleration = 2f;
 	public bool alive;
 	GameObject head;
 	public GameObject shield;
@@ -45,9 +46,13 @@ public class Body : MovingObject {
 				sideMove = joystick.GetTouchPosition.x;
 				straightMove = joystick.GetTouchPosition.y;
 			}
-
-			Vector3 movement = new Vector3 (sideMove, 0.0f, straightMove);
-			rB.AddForce (movement * maxSpeed);
+			float rigidBodyMagnitude = rB.velocity.magnitude;
+			
+			if (rigidBodyMagnitude < maxSpeed) {
+				Vector3 movement = new Vector3 (sideMove, 0.0f, straightMove);
+				rB.AddForce (movement * acceleration);
+			}
+			
 			head.transform.position = new Vector3 (transform.position.x, head.transform.position.y, transform.position.z);
 			if(shield != null) {
 				shield.transform.position = new Vector3 (transform.position.x, head.transform.position.y, transform.position.z);
