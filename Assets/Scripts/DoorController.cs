992be7f022aab99367a347fef0cecda4787a6b41
Cyro Guimaraes <cyro809@gmail.com>
2020-09-19 19:43:59 -0300

    Adds door game object with rotation script.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed;
+    Quaternion target;
+    public float targetDegree;
+    
+    void Start()
+    {
+        target = transform.rotation;
+        target = Quaternion.Euler(0, targetDegree, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target, moveSpeed * Time.deltaTime);
+    }
+
+    void RotateDoor(float degrees) {
+        
+    }
+}
