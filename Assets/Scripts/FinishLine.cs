833d9a9a7882406f54a5108b0d3d9bed1209e2b0
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2020-11-29 19:23:53 -0300

    WIP: new finish line.

---
+++ Assets/Scripts/FinishLine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color col = gameObject.GetComponent<MeshRenderer> ().material.color;
+        col.a = 0.5f;
+        GetComponent<MeshRenderer>().material.color = col;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
