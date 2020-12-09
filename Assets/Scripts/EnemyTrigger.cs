0a53c0f8d5a02162cb2ab526f517ddd9b9be0f2f
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2020-12-09 18:02:18 -0300

    Level 2 Finished. Checking collision once on enemy trigger.

--- Assets/Scripts/EnemyTrigger.cs
+++ Assets/Scripts/EnemyTrigger.cs
@@ -6,12 +6,16 @@ public class EnemyTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] enemiesObject;
+    bool hasEntered = false;
 
     void OnCollisionEnter (Collision col) {
-        if(col.gameObject.CompareTag("Body")) {
+        if(col.gameObject.CompareTag("Body") && !hasEntered) {
             for (int i=0;i < enemiesObject.Length; i++) {
-                enemiesObject[i].GetComponent<Enemy>().TriggerEnemy();
+                if(enemiesObject[i] != null) {
+                    enemiesObject[i].GetComponent<Enemy>().TriggerEnemy();
+                }
             }
+            hasEntered = true;
         }
     }
 }
