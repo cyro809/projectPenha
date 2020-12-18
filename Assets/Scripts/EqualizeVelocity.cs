942c8f6e951ecf0a9b5d0bc772d2dab1462fa7ce
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2020-12-18 14:43:51 -0300

    Small platform prefab. Ball stays on platform while moving.

---
+++ Assets/Scripts/EqualizeVelocity.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EqualizeVelocity : MonoBehaviour
+{
+    private GameObject target = null;
+    private Vector3 offset;
+    void Start()
+    {
+        target = null;
+    }
+    void OnCollisionStay(Collision col)
+    {
+        if ((col.transform.tag == "Body"))
+        {
+            target = col.gameObject;
+            offset = target.transform.position - transform.position;
+        }
+    }
+    void OnCollisionExit(Collision col)
+    {
+        target = null;
+    }
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            target.transform.position = transform.position + offset;
+        }
+    }
+}
