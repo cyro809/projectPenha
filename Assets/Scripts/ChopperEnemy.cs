1dac7a623c3ddaa42c288f040578f750f40d3ea4
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2021-04-27 18:28:54 -0300

    WIP: chopper enemy.

---
+++ Assets/Scripts/ChopperEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopperEnemy : MonoBehaviour
+{
+    public float rotationSpeed = 100;
+    public GameObject enemyBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up *(rotationSpeed * Time.deltaTime));
+        Vector3 enemyPosition = enemyBody.transform.position;
+        transform.position = new Vector3(enemyPosition.x, transform.position.y, enemyPosition.z);
+    }
+}
