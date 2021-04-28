3080821c7c91b86054338a779293cb12e18662d8
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2021-04-28 18:07:06 -0300

    Chopper Enemy Propeller hiting player.

---
+++ Assets/Scripts/PlayerHitableObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitableObject : MonoBehaviour
+{
+   public float pushBackForce;
+   public bool destroyableObject = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if(IsPlayer(col)) {
+			HitPlayer(col);
+		}
+        if(col.gameObject.CompareTag("Bullet") && destroyableObject) {
+            Destroy(gameObject);
+        }
+    }
+
+    void HitPlayer(Collision col) {
+        Body player = col.gameObject.GetComponent<Body>();
+        if(player) {
+            Vector3 pos = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
+            player.getHit (pushBackForce, pos);
+        }
+    }
+
+    bool IsPlayer(Collision col) {
+        return col.gameObject.CompareTag("Body") || col.gameObject.CompareTag("Player");
+    }
+}
