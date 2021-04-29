d001d48ec501f2c38e7221c61ebd304b01173329
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2021-04-29 19:42:55 -0300

    Refactor from Bomb to Grenade. Grenade explosion hitting enemy.

---
+++ Assets/Scripts/GrenadeExplosion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if (IsEnemy(col)) {
+            Enemy hittedEnemy = col.gameObject.GetComponent<Enemy>();
+            hittedEnemy.getHit(3000.0f, transform.position);
+        }
+    }
+
+    bool IsEnemy(Collision col) {
+        return col.gameObject.CompareTag("Enemy") ||
+            col.gameObject.CompareTag("ChopperEnemy") ||
+            col.gameObject.CompareTag("ShootingEnemy");
+    }
+}
