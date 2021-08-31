b9faad9012a5ed890acf0a1aac3e787b69f47cec
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2021-08-31 14:56:11 -0300

    WIP: trying to rotate shooting enemy boss body.

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -8,11 +8,11 @@ public class Patrol : MonoBehaviour {
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
-
+    Rigidbody rB;
 
     void Start () {
         agent = GetComponent<NavMeshAgent>();
-
+        rB = gameObject.GetComponent<Rigidbody>();
         // Disabling auto-braking allows for continuous movement
         // between points (ie, the agent doesn't slow down as it
         // approaches a destination point).
@@ -32,14 +32,45 @@ public class Patrol : MonoBehaviour {
 
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
-        destPoint = (destPoint + 1) % points.Length;
+        int previousPoint = destPoint;
+        System.Random rand = new System.Random();
+        while (previousPoint == destPoint) {
+            destPoint = rand.Next(0, points.Length);
+        }
     }
 
 
     void Update () {
-        // Choose the next destination point when the agent gets
+        // Choose the next destination point when the agent g ets
         // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+
+        Vector3 direction = GetLookAtDirection(agent.destination);
+        if (!agent.pathPending && agent.remainingDistance < 0.5f) {
+
+            Debug.Log(direction);
             GotoNextPoint();
+
+        } else if (agent.remainingDistance >= 0.5f){
+            // rB.AddForce (points[destPoint].position * 2);
+        }
+        rB.AddForce (direction * 20);
+
+    }
+
+    Vector3 GetLookAtDirection(Vector3 nextPoint) {
+		/*
+		Code reference from: https://answers.unity.com/questions/239614/roll-a-ball-towards-the-player.html
+		*/
+
+		Vector3 myPosition = transform.position;
+
+		// work out direction and distance
+		return (nextPoint - myPosition).normalized;
+	}
+
+    void OnCollisionEnter(Collision col) {
+        if(col.gameObject.CompareTag("Bullet")) {
+
+        }
     }
 }
