e4c063c84095fd998955cf6a2079ccba0aa9f8ea
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-01-17 21:21:14 -0300

    WIP: Gun class.

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected int SPECIAL_BULLETS;
+    protected float bulletSpeed;
+    public string gunName;
+    AudioClip shotSound;
+
+    void Start()
+    {
+
+    }
+    public virtual void fire() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
