cdc9bf233410fcf546f4e51e7d2853766181cd6c
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-04-07 15:12:21 -0300

    Adjustings select level button color when disabled

--- Assets/Scripts/LevelsController.cs
+++ Assets/Scripts/LevelsController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using TMPro;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,9 +8,13 @@ public class LevelsController : MonoBehaviour
 {
     int lastClearedLevel;
     public GameObject[] levels;
+    Color disabledTextColor = new Color(0.3f, 0.3f, 0.3f, 1.0f);
+    Color enabledTextColor;
     void Start () {
         lastClearedLevel = PlayerPrefs.GetInt("lastClearedLevel", 0);
         // levels = GameObject.FindGameObjectsWithTag("levelButton");
+        Debug.Log(lastClearedLevel);
+        enabledTextColor = levels[0].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color;
         DisableAllLevels();
         EnableClearedLevels();
         Time.timeScale = 1;
@@ -24,13 +29,17 @@ public class LevelsController : MonoBehaviour
     void DisableAllLevels() {
         for (int i=0; i< levels.Length; i++) {
             levels[i].GetComponent<Button>().interactable = false;
+            levels[i].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = disabledTextColor;
         }
     }
     void EnableClearedLevels() {
         levels[0].GetComponent<Button>().interactable = true;
-        for (int i=0; i<=lastClearedLevel; i++) {
-            levels[i].GetComponent<Button>().interactable = true;
+        levels[0].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = enabledTextColor;
+        if(lastClearedLevel < levels.Length) {
+            for (int i=0; i<=lastClearedLevel; i++) {
+                levels[i].GetComponent<Button>().interactable = true;
+                levels[i].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>().color = enabledTextColor;
+            }
         }
-
     }
 }
