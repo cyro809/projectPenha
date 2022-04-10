767015f39131c099ba720a9a54acec40aba2a511
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-04-10 11:06:46 -0300

    Fading story text

--- Assets/Scripts/StoryTextController.cs
+++ Assets/Scripts/StoryTextController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,14 +6,17 @@ using UnityEngine;
 public class StoryTextController : MonoBehaviour
 {
     // Start is called before the first frame update
+    TextMeshProUGUI text;
+    float speed = 0.001f;
     void Start()
     {
-        
+        text = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        text.color = new Color(1.0f, 1.0f, 1.0f, speed);
+        speed += 0.005f;
     }
 }
