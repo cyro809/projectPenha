58b5fa532041166df85a3a4dd350121c1061134e
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2022-05-18 16:06:37 -0300

    WIP: Feedback modal

---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour
+{
+
+	public void OpenFeedbackForm()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://forms.gle/dytJxvUPhsKFwJA97");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
